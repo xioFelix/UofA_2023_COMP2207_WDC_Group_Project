b42a87475c32395893fee30c191446f2a089d451
Felix <a1878510@adelaide.edu.au>
2023-05-10 10:41:08 +0000

    May 10, 2023, 20:11:08 GMT+9:30

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function (req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-router.get('/contact.ajax', (req, res) => {
-  const contact = '<a href = "https://myuni.adelaide.edu.au">contact us</a>';
-  res.send(contact);
-});
+// router.get('/contact.ajax', (req, res) => {
+//   const contact = '<a href = "https://myuni.adelaide.edu.au">contact us</a>';
+//   res.send(contact);
+// });
 
-router.get('/search.ajax', (req, res) => {
-  const search = '<input type = "text" placeholder = "Search"><button>search</button>';
-  res.send(search);
-});
+// router.get('/search.ajax', (req, res) => {
+//   const search = '<input type = "text" placeholder = "Search"><button>search</button>';
+//   res.send(search);
+// });
 
-router.get('/about.ajax', (req, res) => {
-  const about = '<input type = "text" placeholder = "about"><button>about</button>';
-  res.send(about);
-});
-module.exports = router;
+// router.get('/about.ajax', (req, res) => {
+//   const about = '<input type = "text" placeholder = "about"><button>about</button>';
+//   res.send(about);
+// });
+// module.exports = router;
