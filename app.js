6e6eb58c75dc35867bf0011f4a96980fc4d89941
Felix <a1878510@adelaide.edu.au>
2023-05-30 14:28:46 +0000

    May 30, 2023, 23:58:46 GMT+9:30

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
+const session = require("express-session");
 
 
 const app = express();
@@ -18,5 +18,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(session({
+    // 给cookie中存储的sessionid加密的， 可以随意指定一个字符串
+    secret: 'itcast',
+    // 设置浏览器端cookie中的sessionId设置名字， 默认connect.sid
+    name: 'sessionId',
+    resave: false,
+    // 在浏览器和服务器连接的第一时间，分配session  给浏览器指定一个cookie
+    saveUninitialized: true
+}));
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,15 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 判断登陆中间件
+app.use((req, res, next) => {
+  if (!req.session.user && req.url !== '/login') {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+});
+
 router.post('/loginToUser', function (req, res) {
   res.redirect('./Users/user/home_page.html');
 });
@@ -24,5 +33,22 @@ router.post('/loginToAdmin', function (req, res) {
 router.post('/otherLoginToUser', function (req, res) {
   res.redirect('./Users/user/home_page.html');
 });
+
+// // 登录功能 待实现
+// router.findUser(username, password, result => {
+//   if (result.length > 0) {
+//     // 登录成功
+//     // 登录成功了，把当前用户的信息，保存到req.session中
+//     req.session.user = req.body;
+//     res.redirect('/');
+//   } else {
+//     res.redirect('/login');
+//   }
+// });
+
+// // 退出登录功能 待实现
+// req.session.user = null;
+// // 跳到登录页
+// res.redirect('/login');
 
 module.exports = router;
