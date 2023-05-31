abb539d16f550063f26133a3b4107f511324e3d3
Felix <a1878510@adelaide.edu.au>
2023-05-31 16:15:04 +0000

    Jun 1, 2023, 01:45:04 GMT+9:30

--- routes/index.js
+++ routes/index.js
@@ -20,21 +20,6 @@ router.get('/', function (req, res, next) {
 
 router.post('/loginToUser', function (req, res) {
 
-    const {OAuth2Client} = require('google-auth-library');
-  const client = new OAuth2Client(CLIENT_ID);
-  async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    // const domain = payload['hd'];
-  }
-  verify().catch(console.error);
 
   res.redirect('./Users/user/home_page.html');
 });
