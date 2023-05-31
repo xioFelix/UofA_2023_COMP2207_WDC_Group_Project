5034248ebb0749ae0587aa347dd781f8e5710586
Felix <a1878510@adelaide.edu.au>
2023-05-31 17:20:41 +0000

    Jun 1, 2023, 02:50:41 GMT+9:30

--- app.js
+++ app.js
@@ -19,14 +19,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-app.use(session({
-    // 给cookie中存储的sessionid加密的， 可以随意指定一个字符串
-    secret: 'itcast',
-    // 设置浏览器端cookie中的sessionId设置名字， 默认connect.sid
-    name: 'sessionId',
-    resave: false,
-    // 在浏览器和服务器连接的第一时间，分配session  给浏览器指定一个cookie
-    saveUninitialized: true
-}));
+
 
 module.exports = app;
