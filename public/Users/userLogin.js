4f18f13ed067585d62d50b3d25ef2c944eea09f8
Felix <a1878510@adelaide.edu.au>
2023-05-31 14:35:06 +0000

    2023年6月1日 00:05

--- public/Users/userLogin.js
+++ public/Users/userLogin.js
@@ -1,12 +1,1 @@
-$(document).ready(function () {
-			$('#horizontalTab').easyResponsiveTabs({
-				type: 'default',
-				width: 'auto',
-				fit: true
-			});
-		});
 
-        function google_login(response){
-            console.log(response);
-        }
-        
