3c520d25b8ecbed29aaf15b09319138f53ef9bd2
Felix <a1878510@adelaide.edu.au>
2023-06-02 08:32:56 +0000

    Jun 2, 2023, 18:02:56 GMT+9:30

--- public/Users/user/setting.js
+++ public/Users/user/setting.js
@@ -17,3 +17,57 @@ function openClubUser() {
 function openClubAll() {
     window.location.href = 'club_all.html';
 }
+
+
+// databsases
+document.addEventListener('DOMContentLoaded', () => {
+    function fetchActors() {
+        fetch('/actors')
+            .then((response) => response.json())
+            .then((data) => {
+                const tableBody = document.querySelector('table tbody');
+                tableBody.innerHTML = '';
+
+                data.forEach((actor) => {
+                    const row = document.createElement('tr');
+                    row.innerHTML = `<td>${actor.first_name}</td><td>${actor.last_name}</td>`;
+                    tableBody.appendChild(row);
+                });
+            });
+    }
+
+    fetchActors();
+
+    const addActorForm = document.querySelector('form');
+
+    // eslint-disable-next-line no-shadow
+    addActorForm.addEventListener('submit', (addActorForm) => {
+        addActorForm.preventDefault();
+
+        const firstNameInput = document.querySelector('#actor-first-name');
+        const lastNameInput = document.querySelector('#actor-last-name');
+
+        const actor = {
+            first_name: firstNameInput.value,
+            last_name: lastNameInput.value
+        };
+
+        fetch('/actors', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(actor)
+        })
+            .then((response) => response.json())
+            .then((data) => {
+
+                // eslint-disable-next-line no-console
+                console.log(data);
+            });
+
+        firstNameInput.value = '';
+        lastNameInput.value = '';
+        fetchActors();
+    });
+});
