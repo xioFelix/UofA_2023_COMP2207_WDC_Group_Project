5fbc15361df8222b513542cd0f64d305d416efcb
Felix <a1878510@adelaide.edu.au>
2023-06-02 08:46:39 +0000

    Jun 2, 2023, 18:16:39 GMT+9:30

--- public/Users/user/setting.js
+++ public/Users/user/setting.js
@@ -1,6 +1,6 @@
 const password = document.getElementById('password');
-const anniu = document.getElementById('conceal');
-anniu.addEventListener('click', function () {
+// const anniu = document.getElementById('conceal');
+document.addEventListener('click', function () {
     if (password.type === 'password') {
         password.setAttribute('type', 'text');
     } else {
