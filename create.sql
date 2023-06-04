23e2f7a81a9ecd79ebd59a74ca76b0372b7eee25
Felix <a1878510@adelaide.edu.au>
2023-06-04 14:01:27 +0000

    Update Mysql

--- create.sql
+++ create.sql
@@ -16,6 +16,14 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Current Database: `survival`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `survival` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `survival`;
+
+--
 -- Table structure for table `activity`
 --
 
@@ -138,4 +146,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-06-04 13:41:41
+-- Dump completed on 2023-06-04 14:01:01
