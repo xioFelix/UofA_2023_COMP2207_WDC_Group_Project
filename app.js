7517de2ecbed9448f1f899512d4e8c6b12316437
Felix <a1878510@adelaide.edu.au>
2023-06-07 09:03:33 +0000

    Jun 7, 2023, 18:33:33 GMT+9:30

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ app.get('/set_google_cookie', (req, res) => {
 });
 
 app.get('/get_google_cookie', (req, res) => {
-    const { google_cookie } = req.session.username;
+    const { google_cookie } = req.session;
     res.send('Cookie value: ' + google_cookie);
 });
 
