51a69691dac134a8f27e8ca31420c856f54f5622
Felix <a1878510@adelaide.edu.au>
2023-06-07 12:04:58 +0000

    Jun 7, 2023, 21:34:58 GMT+9:30

--- routes/index.js
+++ routes/index.js
@@ -99,7 +99,7 @@ router.get('/get_user_info', function (req, res, next) {
   const { user_email } = req.session.userEmail;
 
   res.json({
-    user_id: req.session.username,
+    user_id: req.session.user_name,
     user_name: req.session.user_name,
     user_email: req.session.user_email
   });
